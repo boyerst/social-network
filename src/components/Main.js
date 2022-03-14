00b6c499c034c8d8722fc7cd13d50084da32edbf
Steve Boyer <stephen.a.boyer@gmail.com>
2022-03-14 14:02:15 -0500

    Add form for post input

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,24 @@ class Main extends Component {
         <div className="row">
           <main role="main" className="col-lg-12 ml-auto mr-auto" style= {{ maxWidth: '500px' }}>
             <div className="content mr-auto ml-auto">
+            <p>&nbsp;</p>
+                <form onSubmit={(event) => {
+                  event.preventDefault()
+                  const content = this.postContent.value
+                  this.props.createPost(content)
+                }}>
+                <div className="form-group mr-sm-2">
+                  <input
+                    id="postContent"
+                    type="text"
+                    ref={(input) => { this.postContent = input }}
+                    className="form-control"
+                    placeholder="What's on your mind?"
+                    required />
+                </div>
+                <button type="submit" className="btn btn-primary btn-block">Share</button>
+              </form>
+            <p>&nbsp;</p>
             { this.props.posts.map((post, key) => {
               return(
                 <div className="card mb-4" key={key}>
