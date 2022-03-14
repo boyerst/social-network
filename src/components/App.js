21e25a3a4a535d83dceb1381541c14a55c017187
Steve Boyer <stephen.a.boyer@gmail.com>
2022-03-14 13:36:23 -0500

    Refactor posts code into new Main.js component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
+import Identicon from 'identicon.js';
 import './App.css';
 import SocialNetwork from '../abis/SocialNetwork.json'
 import Navbar from './Navbar';
-import Identicon from 'identicon.js';
+import Main from './Main';
+
 
 class App extends Component {
 
@@ -89,44 +91,7 @@ class App extends Component {
     return (
       <div>
         <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style= {{ maxWidth: '500px' }}>
-              <div className="content mr-auto ml-auto">
-              { this.state.posts.map((post, key) => {
-                return(
-                  <div className="card mb-4" key={key}>
-                    <div className="card-header">
-                      <img 
-                        className="mr-2"
-                        width='30'
-                        height='30'
-                        src={`data:image/png;base64,${new Identicon(this.state.account, 30).toString()}`}
-                      />
-                      <small className="text-muted">{post.author}</small>
-                    </div>
-                    <ul id="postList" className="list-group list-group-flush">
-                      <li className="list-group-item">
-                        <p>{post.content}</p>
-                      </li>
-                      <li key={key} className="list-group-item py-2">
-                        <small className="float-left mt-1 text-muted">
-                          TIPS: {window.web3.utils.fromWei(post.tipAmount.toString(), 'Ether')}
-                        </small>
-                        <button className="btn btn-link btn-sm float-right pt-0">
-                          <span>
-                            TIP .1 ETH
-                          </span>
-                        </button>
-                      </li>
-                    </ul>
-                  </div>
-                )
-              })}
-              </div>
-            </main>
-          </div>
-        </div>
+        <Main posts={this.state.posts} />
       </div>
     );
   }
